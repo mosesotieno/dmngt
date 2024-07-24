f797e318d228a4d0fc391f654bffb66c75c1e2f5
Moses Otieno <mosotieno25@gmail.com>
2024-07-24 11:27:07 +0300

    Updated the R file and added more flesh

--- R/anymatch.R
+++ R/anymatch.R
@@ -1,3 +1,17 @@
+#' Create variable from set of variables whose values are not missing
+#'
+#' @param vars A set of variables that you want to create your variable
+#'
+#' @return A binary character vector with Yes or No
+#' @export
+#'
+#' @examples
+#' df <- data.frame (id = 1:10,
+#' var1 = sample(c(1:2,NA), 10, replace=TRUE),
+#' var2 = sample(c(1:2,NA), 10, replace=TRUE),
+#' var3 = sample(c(3:5, NA), 10, replace=TRUE))
+#' df |> dplyr::mutate(v4 = anymatch("var"))
+
 anymatch <- function(vars){
   ifelse(rowSums(!is.na(dplyr::pick(dplyr::starts_with(vars)))), "Yes", "No")
 }
