f945b0e5b2c6077e9bc0293c39d638676b9417dd
Moses Otieno <mosotieno25@gmail.com>
2024-07-24 16:50:35 +0300

    Updated the any_yes function and documentation

--- R/any_yes.R
+++ R/any_yes.R
@@ -1,10 +1,8 @@
 #' Title
 #'
-#' @param .data Dataframe that you want to create the variable in
-#' @param variable The variable name that you want to give to collapsed variable
 #' @param varcollapse The list of variables that you want to use to create the variable mentioned
 #'
-#' @return Dataframe with the newly created variable
+#' @return A binary character vector with Yes or No
 #' @export
 #'
 #' @examples
@@ -12,11 +10,10 @@
 #' var1 = factor(round(runif(20, 1, 3)),labels = c("Yes", "No", "N/A")),
 #' var2 = factor(round(runif(20, 1, 3)),labels = c("Yes", "No", "N/A")),
 #' var3 = factor(round(runif(20, 1, 3)),labels = c("Yes", "No", "N/A")))
-#' df |> any_yes("ouctome", c("var1", "var2", "var3"))
-any_yes <- function(.data, variable, varcollapse){
-  .data %>%
-    mutate(!!variable := ifelse(rowSums(
-      select(., all_of(varcollapse)) == "Yes") > 0, "Yes", "No"))
+#' df |> dplyr::mutate(ouctome =  any_yes(c("var1", "var2", "var3")))
+any_yes <- function(varcollapse){
+
+  ifelse(rowSums(dplyr::pick(dplyr::all_of(varcollapse)) == "Yes") > 0, "Yes", "No")
 
 
 }
